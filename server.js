9da5f6b0d8ae05c853600de7bb8d836db8df9e8f
mentats <michelemariani@protonmail.com>
2021-05-22 14:58:07 +0200

    feat/frontend: serving html file for '/' route

--- server.js
+++ server.js
@@ -4,22 +4,27 @@ const md5 = require("md5")
 const cookieParser = require("cookie-parser")
 const randomString = require("randomstring")
 const router = require("./router/index")
+const path = require("path")
 
 const app = express()
 const PORT = 3010
 
 const bodyParser = require("body-parser")
-app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.urlencoded({extended: false}))
 app.use(cookieParser())
 app.use(bodyParser.json())
 
-router(app,db)
+router(app, db)
 
-db.sequelize.sync({force:true}).then(() => {
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/frontend/index.html"))
+})
+
+db.sequelize.sync({force: true}).then(() => {
     app.listen(PORT, () => {
         console.log(`app started on port ${PORT}`)
     })
 })
 
-const adminCookie =  md5(randomString.generate())
-const userCookie =  md5(randomString.generate())
+const adminCookie = md5(randomString.generate())
+const userCookie = md5(randomString.generate())
