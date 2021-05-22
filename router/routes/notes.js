d7bcb844738b0c6b1b5d929a2327a9fdb36578e9
mentats <michelemariani@protonmail.com>
2021-05-22 14:50:21 +0200

    feat/waf: implemented special chars filter on notes

--- router/routes/notes.js
+++ router/routes/notes.js
@@ -3,27 +3,43 @@ const md5 = require("md5");
 
 module.exports = (app, db) => {
     app.get('/notes', (req, res) => {
-        db.notes.findAll({
-        })
+        db.notes.findAll({})
             .then(notes => {
                 res.json(notes);
             })
-        .catch(err => {
-            res.json({
-                "error":err.message
+            .catch(err => {
+                res.json({
+                    "error": err.message
+                })
             })
-        })
     });
 
     // POST single user
     app.post('/note', (req, res) => {
 
-        let isHacking = false;
+            let isHacking = false;
 
-        const noteData = {
-         title: req.body.title,
-            content: req.body.content,
-            css: req.body.css,
-        };
-    });
-};
+            const noteData = {
+                title: req.body.title,
+                content: req.body.content,
+                css: req.body.css,
+            };
+
+            var filter = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+            isHacking = filter.test(noteData.title + noteData.content)
+            if (!!isHacking) {
+                res.status(403).json({
+                    "message": "WAF Exception: no special chars allowed."
+                })
+            } else {
+                db.notes.create(noteData)
+                    .then(newNote => {
+                        res.json({
+                            "message": "Note created succesfully. Thanks for using aa",
+                            "noteId": newNote.id
+                        })
+                    })
+            }
+        }
+    )
+}
