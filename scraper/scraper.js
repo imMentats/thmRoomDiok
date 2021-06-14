99207178fa7414555ed82c05f485a72512e2b6fb
mentats <michelemariani@protonmail.com>
2021-06-14 22:55:17 +0200

    at this point this repo is useless, but everything works here.

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -1,5 +1,6 @@
 const puppeteer = require("puppeteer")
 const axios = require("axios")
+const chalk = require("chalk")
 
 const url = process.argv[2];
 if (!url) {
@@ -15,16 +16,30 @@ const flagVisit = (uid) => {
 
 
 async function checkUrls(url) {
+    console.log(chalk.red(url))
     return new Promise(async (resolve, reject) => {
         const browser = await puppeteer.launch({
-            headless: true,
+            headless: false,
             args: ['--disable-web-security'],
             ignoreHTTPSErrors: true
         });
         try {
+            const cookies = [
+                {
+                    "domain": "127.0.0.1:3010", // google.com, yahoo.com etc. Without the host
+                    "name": "access", // here is the actual cookie name
+                    "path": "/",
+                    "value": "03124f34-8f0a-40f4-bd91-fc1ef3c1c611", // and the value
+                    "id": 1
+                }
+            ]
+
             const page = await browser.newPage()
             console.log(url)
-            const response = await page.goto(url, {timeout: 2000})
+            let response = await page.goto(url, {timeout: 2000})
+            await page.setCookie(...cookies)
+            await page.click('input[type="submit"]');
+            await console.log(response)
             page.close()
             browser.close()
             return resolve(response)
@@ -48,14 +63,16 @@ axios.get("http://127.0.0.1:3010/messages")
                             axios.post(`http://127.0.0.1:3010/message`, {
                                     body: `Sorry, but I'm not authorized to visit non secure websites outside the company network.`,
                                     fromAdmin: true,
-                                    relatedTo: message.uid
-                                },
+                                    relatedTo: message.uid,
+                                    token: "03124f34-8f0a-40f4-bd91-fc1ef3c1c611"
+                                }
                             ).then(res => {
                                 console.log(message.uid)
                             }).catch(err => {
                                 console.log(err)
                             })
-                        } else { //Not https
+                        } else { // HTTPS
+                            console.log(chalk.blue("url in else statement: " + message.url))
                             checkUrls(message.url)
                                 .then(res => {
                                     // csrf page response status
@@ -63,7 +80,8 @@ axios.get("http://127.0.0.1:3010/messages")
                                         axios.post(`http://127.0.0.1:3010/message`, {
                                                 body: `I've taken a look at the page you sent me but I'm not sure about what you're trying to tell me. I will probably (hopefully) get in contact with you tomorrow.`,
                                                 fromAdmin: true,
-                                                relatedTo: message.uid
+                                                relatedTo: message.uid,
+                                                token: "03124f34-8f0a-40f4-bd91-fc1ef3c1c611"
                                             },
                                         ).then(() => {
                                             console.log("visited OK")
@@ -79,7 +97,8 @@ axios.get("http://127.0.0.1:3010/messages")
                                     axios.post(`http://127.0.0.1:3010/message`, {
                                             body: `I tried visiting the website but it seems broken...`,
                                             fromAdmin: true,
-                                            relatedTo: message.uid
+                                            relatedTo: message.uid,
+                                            token: "03124f34-8f0a-40f4-bd91-fc1ef3c1c611"
                                         },
                                     ).then(() => {
                                         console.log("visited OK")
@@ -98,7 +117,8 @@ axios.get("http://127.0.0.1:3010/messages")
                             axios.post(`http://127.0.0.1:3010/message`, {
                                     body: `The url doesn't seems to be valid. Double check stuff before submitting them.`,
                                     fromAdmin: true,
-                                    relatedTo: message.uid
+                                    relatedTo: message.uid,
+                                    token: "03124f34-8f0a-40f4-bd91-fc1ef3c1c611"
                                 },
                             ).then(() => {
                                 flagVisit(message.uid)
@@ -110,12 +130,13 @@ axios.get("http://127.0.0.1:3010/messages")
                     }
                 }
             }
-        })()
+        })
+        ()
 
     }).catch(err => {
     console.log(err)
 }).finally(() => {
-    axios.get("http://localhost:3010/aaa/1")
+    axios.get("http://127.0.0.1:3010/aaa/1")
         .then(res => {
             console.log("health check")
         })
